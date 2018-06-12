c6394473e93ef8de93b069f8347a7ff86919080f
Fabio Ribeiro <fabio@dogandponystudios.com>
2018-06-12 17:47:54 -0300

    Init only on project

--- src/js/init.libs.js
+++ src/js/init.libs.js
@@ -1,28 +1,28 @@
-var Sushi;
+// var Sushi;
 
-(function (Sushi) {
-	"use strict";
+// (function (Sushi) {
+// 	"use strict";
 
-	var Util = Sushi.Util;
+// 	var Util = Sushi.Util;
 
-	Sushi.init = function (parentElement) {
-		var pluggableElements = parentElement.querySelectorAll("[data-sushi]");
+// 	Sushi.init = function (parentElement) {
+// 		var pluggableElements = parentElement.querySelectorAll("[data-sushi]");
 
-		for (var i in pluggableElements) {
-			if (pluggableElements.hasOwnProperty(i)) {
-				var element = pluggableElements[i];
-				var pluginName = element.dataset.sushi;
-				var pluginNameCamelCase = Util.firstCharacterToLowerCase(pluginName);
+// 		for (var i in pluggableElements) {
+// 			if (pluggableElements.hasOwnProperty(i)) {
+// 				var element = pluggableElements[i];
+// 				var pluginName = element.dataset.sushi;
+// 				var pluginNameCamelCase = Util.firstCharacterToLowerCase(pluginName);
 
-				element._sushi || (element._sushi = {});
+// 				element._sushi || (element._sushi = {});
 
-				if (element._sushi[pluginNameCamelCase] === void 0) {
-					element._sushi[pluginNameCamelCase]
-						= new Sushi.Plugins[pluginName](element);
-				}
-			}
-		}
-	};
+// 				if (element._sushi[pluginNameCamelCase] === void 0) {
+// 					element._sushi[pluginNameCamelCase]
+// 						= new Sushi.Plugins[pluginName](element);
+// 				}
+// 			}
+// 		}
+// 	};
 
-	Sushi.init(document);
-})(Sushi || (Sushi = {}));
+// 	Sushi.init(document);
+// })(Sushi || (Sushi = {}));
