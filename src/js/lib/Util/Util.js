97e340455b0da3078e43c7baca5a158d8b5abc0a
Caio Costa <caiocrcosta@gmail.com>
2018-06-20 15:54:33 -0300

    Remove getYouTubeVideoID()
    Method is way too opinionated to be here

--- src/js/lib/Util/Util.js
+++ src/js/lib/Util/Util.js
@@ -111,19 +111,6 @@ var Sushi;
 
 
 	/**
-	 * JavaScript function to match (and return) the video Id
-	 * of any valid Youtube Url, given as input string.
-	 * @author: Stephan Schmitz <eyecatchup@gmail.com>
-	 * @url: http://stackoverflow.com/a/10315969/624466
-	 */
-	Util.getYouTubeVideoID = function (url) {
-		var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-
-		return (url.match(p)) ? RegExp.$1 : false;
-	};
-
-
-	/**
 	 * Returns a single transition end event supported by the current browser
 	 * @returns {*}
 	 */
