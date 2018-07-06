6504ba073724b2e53eda1e1cfe0fd6118ece8e0e
Caio Costa <caiocrcosta@gmail.com>
2018-07-06 17:57:12 -0300

    Re-implement parsing of HTML strings for compatibility with IE
    Also changes the `onlyFirst` parameter to `onlyOne`.

--- src/js/lib/Dom.js
+++ src/js/lib/Dom.js
@@ -22,6 +22,8 @@ var Sushi;
 (function (Sushi) {
 	"use strict";
 
+	var Util = Sushi.Util;
+
 	var Dom = function () {};
 
 	var traverseClassNames = function (target, method, classNames) {
@@ -43,9 +45,72 @@ var Sushi;
 		}
 	};
 
-	Dom.get = function (selector, context, onlyFirst) {
+	/**
+	 * @url https://gist.github.com/Munawwar/6e6362dbdf77c7865a99
+	 *
+	 * @param element
+	 * @param context
+	 * @returns {DocumentFragment}
+	 */
+	var parseHtmlUsingFragment = function (element, context) {
+		context = context || document;
+
+		var rxhtmlTag = /<(?!area|br|col|embed|hr|img|input|link|meta|param)(([\w:]+)[^>]*)\/>/gi;
+		var rtagName = /<([\w:]+)/;
+		var rhtml = /<|&#?\w+;/;
+		// We have to close these tags to support XHTML (#13200)
+		var wrapMap = {
+			// Support: IE9
+			option: [1, "<select multiple='multiple'>", "</select>"],
+
+			thead: [1, "<table>", "</table>"],
+			col: [2, "<table><colgroup>", "</colgroup></table>"],
+			tr: [2, "<table><tbody>", "</tbody></table>"],
+			td: [3, "<table><tbody><tr>", "</tr></tbody></table>"],
+
+			_default: [0, "", ""],
+		};
+
+		var tmp;
+		var tag;
+		var wrap;
+		var j;
+		var fragment = context.createDocumentFragment();
+
+		if (!rhtml.test(element)) {
+			fragment.appendChild(context.createTextNode(element));
+
+			// Convert html into DOM nodes
+		}
+		else {
+			tmp = fragment.appendChild(context.createElement("div"));
+
+			// Deserialize a standard representation
+			tag = (rtagName.exec(element) || ["", ""])[1].toLowerCase();
+			wrap = wrapMap[tag] || wrapMap._default;
+			tmp.innerHTML = wrap[1] + element.replace(rxhtmlTag, "<$1></$2>") + wrap[2];
+
+			// Descend through wrappers to the right content
+			j = wrap[0];
+
+			while (j--) {
+				tmp = tmp.lastChild;
+			}
+
+			// Remove wrappers and append created nodes to fragment
+			fragment.removeChild(fragment.firstChild);
+
+			while (tmp.firstChild) {
+				fragment.appendChild(tmp.firstChild);
+			}
+		}
+
+		return fragment;
+	};
+
+	Dom.get = function (selector, context, onlyOne) {
 		if (selector instanceof HTMLElement) {
-			if (onlyFirst === true) {
+			if (onlyOne === true) {
 				return selector;
 			}
 
@@ -55,10 +120,10 @@ var Sushi;
 			selector = selector.trim();
 
 			if (selector.indexOf("<") === 0) {
-				return Dom.parse(selector, onlyFirst);
+				return onlyOne ? Dom.parseOne(selector) : Dom.parse(selector);
 			}
 
-			return Dom.query(selector, context, onlyFirst);
+			return Dom.query(selector, context, onlyOne);
 		}
 	};
 
@@ -66,40 +131,55 @@ var Sushi;
 		return Dom.get(selector, context, true);
 	};
 
-	Dom.parse = function (html, onlyFirst) {
-		html = html.trim();
+	Dom.parse = function (html, returnFragment) {
+		returnFragment = returnFragment || false;
 
-		var firstTag = html.match(/<(.*?)[\s>]/)[1];
-		var range = document.createRange();
+		var parent;
 
-		range.selectNode(document.getElementsByTagName(firstTag).item(0));
+		if (window.HTMLTemplateElement === void 0) {
+			parent = parseHtmlUsingFragment(html);
+		}
+		else {
+			var templateElement = document.createElement("template");
 
-		var parent = range.createContextualFragment(html);
+			templateElement.innerHTML = html;
 
-		return onlyFirst ? parent.firstChild : parent.children;
+			parent = templateElement.content;
+		}
+
+		return returnFragment ? parent : parent.childNodes;
 	};
 
 	Dom.parseOne = function (html) {
-		return Dom.parse(html, true);
+		return Dom.parse(html).item(0);
 	};
 
-	Dom.query = function (selector, context, onlyFirst) {
+	Dom.query = function (selector, context, onlyOne) {
 		selector = selector.trim();
 		context = context || document;
-		onlyFirst = onlyFirst || false;
+		onlyOne = onlyOne || false;
+
+		var isDirectChildSelector = (selector.indexOf(">") === 0);
+
+		if (isDirectChildSelector && (context instanceof HTMLElement)) {
+			if (context.id === "") {
+				context.id = Util.uniqueId("__sushi");
+			}
+
+			return Dom.query("#" + context.id + " " + selector, document, onlyOne);
+		}
 
 		var isSimpleSelector = (/[^a-zA-Z#.]/.test(selector) === false);
-
 		var isIdSelector = (isSimpleSelector && (selector.indexOf("#") === 0));
 		var isClassSelector = (isSimpleSelector && !isIdSelector && (selector.indexOf(".") === 0));
 		var isTagSelector = (isSimpleSelector && !isIdSelector && !isClassSelector);
 
-		if (onlyFirst === true) {
+		if (onlyOne === true) {
 			if (isIdSelector) {
 				return document.getElementById(selector.substring(1));
 			}
 			else if (isClassSelector) {
-				return context.getElementsByClassName(selector.substring(1))[0] || null;
+				return context.getElementsByClassName(selector.substring(1)).item(0);
 			}
 			else if (isTagSelector) {
 				return context.getElementsByTagName(selector)[0] || null;
