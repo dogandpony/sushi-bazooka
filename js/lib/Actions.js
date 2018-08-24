48307fb358ede905f4f7a6cb0096a6837a79ba2e
Caio Costa <caiocrcosta@gmail.com>
2018-08-24 13:38:05 -0300

    Fix controller parsing; remove dependency of script ordering

--- js/lib/Actions.js
+++ js/lib/Actions.js
@@ -7,7 +7,6 @@ var Sushi;
 (function (Sushi, root) {
 	"use strict";
 
-	var Events = Sushi.Events;
 	var Util = Sushi.Util;
 
 	var Actions = function (element, actions) {
@@ -22,22 +21,20 @@ var Sushi;
 				var controller = Actions.parseController(eventConfig[eventType]);
 
 				if (controller !== null) {
-					Events(element).off(eventType, controller);
-					Events(element).on(eventType, controller);
+					Sushi.Events(element).off(eventType, controller);
+					Sushi.Events(element).on(eventType, controller);
 				}
 			}
 		}
 	};
 
 	Actions.parseController = function (controllerReference) {
-		var controller = null;
-
 		var reference = controllerReference.split(".");
-		var controllerTree = root;
+		var controller = root;
 		var controllerTreeString = "";
 
 		for (var i = 0; i < reference.length; i++) {
-			controller = controllerTree[reference[i]];
+			controller = controller[reference[i]];
 			controllerTreeString += "." + reference[i];
 
 			if (controller == null) {
