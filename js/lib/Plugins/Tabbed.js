f82762b10555a263e11fb87b09f6b3e4546e20fe
Caio Costa <caiocrcosta@gmail.com>
2019-03-28 22:15:45 -0300

    Add support for push states and hash changes

--- js/lib/Plugins/Tabbed.js
+++ js/lib/Plugins/Tabbed.js
@@ -31,43 +31,44 @@ var Sushi;
 	"use strict";
 
 	var BasePlugin = Plugins.BasePlugin;
-	var Dom = Sushi.Dom;
 	var Events = Sushi.Events;
 
 	var Tabbed = function (triggerElement, options) {
 		BasePlugin.call(this, triggerElement, options);
 
-		var navElement = triggerElement.getElementsByClassName("c-tabbed__nav")[0];
-		var tabContainer = triggerElement.getElementsByClassName("c-tabbed__tabs")[0];
+		this.validateOptions();
+
+		var navElement = triggerElement.getElementsByClassName("c-tabbed__nav").item(0);
+		var tabContainer = triggerElement.getElementsByClassName("c-tabbed__tabs").item(0);
 
 		this.navItems = navElement.getElementsByClassName("c-tabbed__navItem");
 		this.navLinks = navElement.getElementsByClassName("c-tabbed__navLink");
 		this.tabs = tabContainer.getElementsByClassName("c-tabbed__tab");
 
+		this.history = new Sushi.History({
+			type: this.options.navigationType,
+			baseUrl: this.options.baseUrl,
+		});
+
+		this.activateTab(this.parseSlugFromUrl());
 		this.registerListeners();
 	};
 
 	Tabbed.displayName = "Tabbed";
 
-	Tabbed.DEFAULTS = {};
+	Tabbed.DEFAULTS = {
+		navigationType: "hash", // "hash", "pushState", null
+		baseUrl: null,
+	};
 
 	function onNavLinkClick(event) {
+		if (this.options.navigationType !== null) {
+			event.preventDefault();
+		}
+
 		var navItem = event.target.closest(".c-tabbed__navItem");
 
-		Dom.removeClass(this.navItems, "is-active");
-
-		navItem.classList.add("is-active");
-
-		for (var i = 0; i < this.tabs.length; i++) {
-			var tab = this.tabs[i];
-
-			if (tab.dataset.tab === navItem.dataset.tab) {
-				tab.classList.add("is-active");
-			}
-			else {
-				tab.classList.remove("is-active");
-			}
-		}
+		this.history.push(navItem.dataset.tab);
 	}
 
 	Tabbed.prototype = Object.create(BasePlugin.prototype);
@@ -76,8 +77,67 @@ var Sushi;
 
 	proto.constructor = Tabbed;
 
+	proto.validateOptions = function () {
+		if ((this.options.navigationType === "pushState") && !this.options.baseUrl) {
+			// eslint-disable-next-line no-console
+			console.warn(
+				"Option \"navigationType\" is set to \"pushState\", but \"baseUrl\" was not set."
+				+ " Navigation Type option will revert to \"hash\" instead."
+			);
+
+			this.options.navigationType = "hash";
+		}
+
+		return true;
+	};
+
 	proto.registerListeners = function () {
-		Events(this.navLinks).on("Tabbed.click", onNavLinkClick.bind(this));
+		this.history.on(function () {
+			this.activateTab(this.parseSlugFromUrl());
+		}.bind(this));
+
+		Events(this.navLinks).on(this.id + ".Tabbed.click", onNavLinkClick.bind(this));
+	};
+
+	proto.parseSlugFromUrl = function () {
+		if (this.options.navigationType === null) {
+			return null;
+		}
+
+		if (this.options.navigationType === "hash") {
+			return window.location.hash.toString().replace(/^#\/(.*)$/, "$1");
+		}
+		else if (this.options.navigationType === "pushState") {
+			return window.location.toString().replace(this.options.baseUrl + "/", "");
+		}
+	};
+
+	proto.activateTab = function (slug) {
+		if (!slug) {
+			return;
+		}
+
+		for (var i = 0; i < this.tabs.length; i++) {
+			var tab = this.tabs[i];
+
+			if (tab.dataset.tab === slug) {
+				tab.classList.add("is-active");
+			}
+			else {
+				tab.classList.remove("is-active");
+			}
+		}
+
+		for (var j = 0; j < this.navItems.length; j++) {
+			var navItem = this.navItems[j];
+
+			if (navItem.dataset.tab === slug) {
+				navItem.classList.add("is-active");
+			}
+			else {
+				navItem.classList.remove("is-active");
+			}
+		}
 	};
 
 	Plugins.Tabbed = Tabbed;
