fc084111127fd0cbe4b144ec6697393e951b6d95
Caio Costa <caiocrcosta@gmail.com>
2019-08-21 17:47:19 -0300

    Implement automatic repositioning

--- js/lib/Plugins/Tooltip.js
+++ js/lib/Plugins/Tooltip.js
@@ -9,8 +9,7 @@ var Sushi;
 
 	var BasePlugin = Plugins.BasePlugin;
 	var Events = Sushi.Events;
-
-	var BASE_CLASSNAME = "c-tooltip";
+	var Css = Sushi.Util.Css;
 
 	var Tooltip = function (triggerElement, options) {
 		BasePlugin.call(this, triggerElement, options);
@@ -29,6 +28,7 @@ var Sushi;
 		delay: 0,
 		position: "top",
 		triggerEvent: "click",
+		horizontalMargin: 24,
 	};
 
 	Tooltip.prototype = Object.create(BasePlugin.prototype);
@@ -38,18 +38,31 @@ var Sushi;
 	proto.constructor = Tooltip;
 
 	proto.registerEventListeners = function () {
-		Events.on(this.options.triggerEvent, this.triggerElement, this.toggle.bind(this));
+		var events = this.options.triggerEvent.split(" ");
+
+		if (events.length === 1) {
+			Events(this.triggerElement).on(this.options.triggerEvent, this.toggle.bind(this));
+		}
+		else {
+			Events(this.triggerElement)
+				.on(events[0], this.open.bind(this))
+				.on(events[1], this.close.bind(this));
+		}
 	};
 
 	proto.createTooltip = function () {
-		var contentElement = document.createElement("div");
-
-		contentElement.classList.add(BASE_CLASSNAME + "__content");
-		contentElement.innerHTML = this.options.content;
-
 		this.tooltip = document.createElement("div");
-		this.tooltip.appendChild(contentElement);
-		this.tooltip.classList.add(BASE_CLASSNAME);
+		this.tooltip.classList.add("c-tooltip");
+
+		this.arrow = document.createElement("i");
+		this.arrow.classList.add("c-tooltip__arrow");
+
+		this.content = document.createElement("div");
+		this.content.classList.add("c-tooltip__content");
+		this.content.innerHTML = this.options.content;
+
+		this.tooltip.appendChild(this.arrow);
+		this.tooltip.appendChild(this.content);
 
 		this.triggerElement.appendChild(this.tooltip);
 	};
@@ -64,39 +77,117 @@ var Sushi;
 	};
 
 	proto.open = function () {
+		if (this.isOpen) {
+			return;
+		}
+
 		this.isOpen = true;
 
-		setTimeout(function () {
-			if (this.isOpen) {
-				this.tooltip.classList.add("is-open");
+		Events(document).off(this.id + ".Tooltip.click");
 
-				this.centerTooltip();
+		this.tooltip.classList.add("is-visible");
+
+		setTimeout(function () {
+			if (!this.isOpen) {
+				return;
 			}
+
+			this.tooltip.classList.add("is-open");
+
+			this.centerTooltip();
+
+			Events(window).on(this.id + ".Tooltip.resize", function () {
+				this.centerTooltip();
+			}.bind(this));
+
+			if (this.options.triggerEvent !== "click") {
+				return;
+			}
+
+			Events(document).one(this.id + ".Tooltip.click", function () {
+				this.close();
+			}.bind(this));
 		}.bind(this), this.options.delay);
 	};
 
 	proto.close = function () {
+		if (!this.isOpen) {
+			return;
+		}
+
 		this.isOpen = false;
 
 		this.tooltip.classList.remove("is-open");
+
+		clearTimeout(this.closeTimeout);
+
+		this.closeTimeout = setTimeout(function () {
+			if (this.isOpen) {
+				return;
+			}
+
+			this.tooltip.classList.remove("is-visible");
+
+			Events(this.triggerElement).trigger("close");
+
+			Events(window).off(this.id + ".Tooltip.resize");
+			Events(document).off(this.id + ".Tooltip.click");
+		}.bind(this), Css.getMaxTransitionDuration(this.tooltip));
 	};
 
 	proto.addPositionClass = function () {
-		this.tooltip.classList.add(BASE_CLASSNAME + "--" + this.options.position);
+		this.tooltip.classList.add("c-tooltip" + "--" + this.options.position);
 	};
 
 	proto.centerTooltip = function () {
+		var bodyRect = document.body.getBoundingClientRect();
+		var triggerElementRect = this.triggerElement.getBoundingClientRect();
+		var tooltipRect = this.tooltip.getBoundingClientRect();
+
+		var triggerElementCenter = {
+			x: triggerElementRect.x + (triggerElementRect.width / 2),
+			y: triggerElementRect.y + (triggerElementRect.height / 2),
+		};
+
+		var maxWidth = (bodyRect.width - (this.options.horizontalMargin * 2));
+
+		// Shrink the tooltip if it's wider than the max width (window width minus horizontal
+		// margin)
+		if (tooltipRect.width > maxWidth) {
+			this.tooltip.style.width = maxWidth + "px";
+
+			// update rect
+			tooltipRect = this.tooltip.getBoundingClientRect();
+		}
+
+		var centerXMin = (tooltipRect.width / 2) + this.options.horizontalMargin;
+		var centerXMax = (bodyRect.width
+			- (tooltipRect.width / 2)
+			- this.options.horizontalMargin
+		);
+
+		var xOffset = Math.min(
+			Math.max(0, (centerXMin - triggerElementCenter.x)),
+			(centerXMax - triggerElementCenter.x)
+		);
+
+		this.arrow.style.marginLeft = (-1 * xOffset) + "px";
+
 		switch (this.options.position) {
 			case "top":
+			// fall through
+
 			case "bottom":
-				this.tooltip.style.marginLeft =
-					(this.tooltip.getBoundingClientRect().width / -2) + "px";
+				this.tooltip.style.marginLeft = ((tooltipRect.width / -2) + xOffset) + "px";
+
 				break;
 
 			case "left":
+			// fall through
+
 			case "right":
-				this.tooltip.style.marginTop =
-					(this.tooltip.getBoundingClientRect().height / -2) + "px";
+				this.tooltip.style.marginTop = (tooltipRect.height / -2) + "px";
+
 				break;
 		}
 	};
