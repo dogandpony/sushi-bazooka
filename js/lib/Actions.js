f9dd81222f7f2d63e1242bcef3a535fdac57b3ec
Caio Costa <caiocrcosta@gmail.com>
2019-08-21 17:49:08 -0300

    Only parse controllers when events are fired

--- js/lib/Actions.js
+++ js/lib/Actions.js
@@ -2,8 +2,6 @@
  * ACTIONS
  * ============================================================================================== */
 
-var Sushi;
-
 (function (Sushi, root) {
 	"use strict";
 
@@ -16,39 +14,28 @@ var Sushi;
 			actions
 		);
 
-		for (var eventType in eventConfig) {
-			if (eventConfig.hasOwnProperty(eventType)) {
+		Object.keys(eventConfig).forEach(function (eventType) {
+			var eventHandler = function (event) {
 				var controller = Actions.parseController(eventConfig[eventType]);
 
-				if (controller !== null) {
-					Sushi.Events(element).off(eventType, controller);
-					Sushi.Events(element).on(eventType, controller);
-				}
-			}
-		}
+				return controller.call(this, event);
+			};
+
+			Sushi.Events(element).off("actions." + eventType);
+			Sushi.Events(element).on("actions." + eventType, eventHandler);
+		});
 	};
 
 	Actions.parseController = function (controllerReference) {
 		var reference = controllerReference.split(".");
 		var controller = root;
-		var controllerTreeString = "";
 
 		for (var i = 0; i < reference.length; i++) {
 			controller = controller[reference[i]];
-			controllerTreeString += "." + reference[i];
-
-			if (controller == null) {
-				// eslint-disable-next-line no-console
-				console.warn(
-					"Function " + controllerTreeString.replace(/^\./, "") + " doesn't exist."
-				);
-
-				return null;
-			}
 		}
 
 		return controller;
 	};
 
 	Sushi.Actions = Actions;
-})(Sushi || (Sushi = {}), this);
+})(window.Sushi || (window.Sushi = {}), window);
